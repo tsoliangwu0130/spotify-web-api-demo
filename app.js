8849d6530620368fd3ec703a46bcb75bd6888abd
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-08-02 09:08:41 -0700

    Add prompt message

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.listen(8888);
+app.listen(8888, () => {
+    console.log('Server is up on http://localhost:8888');
+});
